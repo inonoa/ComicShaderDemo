8adbd4fb5332fca9dbb463bd2f982dc9d7a8ead9
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-07-22 10:05:57 +0900

    CommandBuffer差し込み

---
+++ Assets/Scripts/InsertBuffer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class InsertBuffer : MonoBehaviour
+{
+    [SerializeField] Material postProcessMat;
+
+    void Start()
+    {
+        CommandBuffer buffer = CreateBuffer("Comical");
+        SetBlit(buffer, postProcessMat, "PostProcess");
+        GetComponent<Camera>().AddCommandBuffer(CameraEvent.BeforeImageEffects, buffer);
+    }
+
+    CommandBuffer CreateBuffer(string name){
+        CommandBuffer buf = new CommandBuffer();
+        buf.name = name;
+        return buf;
+    }
+
+    void SetBlit(CommandBuffer buffer, Material material, string name){
+        int id = Shader.PropertyToID(name);
+
+        buffer.GetTemporaryRT(id, -1, -1);
+        buffer.Blit(BuiltinRenderTextureType.CameraTarget, id);
+        buffer.Blit(id, BuiltinRenderTextureType.CameraTarget, material);
+        buffer.ReleaseTemporaryRT(id);
+    }
+}
